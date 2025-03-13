3145bae8f698ce6f02a194ee641a473f603ea147
cruzjeff225 <cruzjeff225>
2025-03-13 17:36:50 -0600

    Agregando logica de Datos y Negocio para la función credenciales

---
+++ Capa_Negocio/negocioCredenciales.cs
@@ -0,0 +1,35 @@
+﻿using Capa_Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocio
+{
+    public class negocioCredenciales
+    {
+        Credenciales credencialesN = new Credenciales();
+
+        public DataTable obtenerCredenciales()
+        {
+            return credencialesN.obtenerCredenciales();
+        }
+
+        public bool insertarCredenciales(string Plataforma, string Usuario, string Contraseña, int idUsuario)
+        {
+            return credencialesN.insertarCredenciales(Plataforma, Usuario, Contraseña, idUsuario);
+        }
+
+        public bool actualizarCredenciales(int idCredencial, string Plataforma, string Usuario, string Contraseña) 
+        {
+            return credencialesN.actualizarCredenciales(idCredencial, Plataforma, Usuario, Contraseña);
+        }
+
+        public bool eliminarCredenciales(int idCredencial)
+        {
+            return credencialesN.eliminarCredenciales(idCredencial);
+        }
+    }
+}
